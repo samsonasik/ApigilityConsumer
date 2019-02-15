5fd3b5fa571efd3da8b47b45db2d3ff222f1ff61
Abdul Malik Ikhsan <samsonasik@gmail.com>
2019-02-15 13:15:00 +0700

    use null coalesce operator

--- src/Service/ClientServiceFactory.php
+++ src/Service/ClientServiceFactory.php
@@ -13,9 +13,7 @@ class ClientServiceFactory
     {
         $config     = $container->get('config');
         $httpClient = new HttpClient();
-        $authConfig = (! empty($config['apigility-consumer']['auth']))
-            ? $config['apigility-consumer']['auth']
-            : [];
+        $authConfig = $config['apigility-consumer']['auth'] ?? [];
 
         return new ClientService(
             $config['apigility-consumer']['api-host-url'],
