ccaa0ca147cdca6727be0656f67b554266c5aec7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-09 01:45:45 +0700

    php 8 support

--- spec/Result/ClientAuthResultSpec.php
+++ spec/Result/ClientAuthResultSpec.php
@@ -19,12 +19,10 @@ describe('ClientAuthResult', function () {
 
         it('cannot create instance via new ClientAuthResult()', function () {
 
-            skipIf(PHP_MAJOR_VERSION < 7);
-
             try {
                 new ClientAuthResult();
             } catch (Error $e) {
-                expect($e->getMessage())->toBe("Call to private ApigilityConsumer\\Result\\ClientAuthResult::__construct() from context 'Kahlan\\Cli\\Kahlan'");
+                expect($e->getMessage())->toBe("Call to private ApigilityConsumer\\Result\\ClientAuthResult::__construct() from scope Kahlan\\Cli\\Kahlan");
             }
 
         });
