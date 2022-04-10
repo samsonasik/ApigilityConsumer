353f8db01683ca99e2bf411dde962adb1eb01fa7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-10 13:18:09 +0700

    apply new RectorConfig

--- rector.php
+++ rector.php
@@ -1,11 +1,11 @@
 <?php
 
+use Rector\Config\RectorConfig;
 use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (RectorConfig $containerConfigurator): void {
     $containerConfigurator->import(SetList::CODING_STYLE);
     $containerConfigurator->import(SetList::CODE_QUALITY);
     $containerConfigurator->import(SetList::EARLY_RETURN);
@@ -15,7 +15,12 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(SetList::TYPE_DECLARATION_STRICT);
 
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [__DIR__ . '/config', __DIR__ . '/src', __DIR__ . '/spec']);
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/spec',
+        __DIR__ . '/rector.php'
+    ]);
 
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
 };
