07584b701bcbd0120928ea1151f7b17c72c2470f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-14 00:19:58 +0700

    use RectorConfig

--- rector.php
+++ rector.php
@@ -1,26 +1,25 @@
 <?php
 
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $containerConfigurator): void {
-    $containerConfigurator->import(SetList::CODING_STYLE);
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::EARLY_RETURN);
-    $containerConfigurator->import(SetList::NAMING);
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
-    $containerConfigurator->import(SetList::TYPE_DECLARATION);
-    $containerConfigurator->import(SetList::TYPE_DECLARATION_STRICT);
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([
+        SetList::CODING_STYLE,
+        SetList::CODE_QUALITY,
+        SetList::EARLY_RETURN,
+        SetList::NAMING,
+        LevelSetList::UP_TO_PHP_80,
+        SetList::TYPE_DECLARATION,
+        SetList::TYPE_DECLARATION_STRICT
+    ]);
 
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
+    $rectorConfig->paths([
         __DIR__ . '/config',
         __DIR__ . '/src',
         __DIR__ . '/spec',
         __DIR__ . '/rector.php'
     ]);
-
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $rectorConfig->importNames();
 };
